292b2d64d82291dbde92dc5fb343f93c383cc5f8
SarahNakamura <snakamura@unomaha.edu>
2023-11-30 21:39:28 -0600

    change function order

--- final_1_logic.py
+++ final_1_logic.py
@@ -5,56 +5,13 @@ from GPA import *
 class Logic (QMainWindow, Ui_MainWindow):
     student_num = 0
     score_list = []
+
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.setupUi(self)
 
         self.submit_student_buttom.clicked.connect(lambda: self.student_submit())
         self.submit_final_button.clicked.connect(lambda:self.final_submit())
-
-
-
-# returning a score list to use it to determining GPA
-    def student_score(self):
-        student_number = self.student_num
-        #score = input(f'Enter {student_num} score(s): ')
-        score_list = score.split()
-        if len(score_list) > student_number:
-            score_list = score_list[:student_number]
-        else:
-            while len(score_list) < student_number:
-                score = input(f'Enter {student_number} score(s): ')
-                score_list = score.split()
-        return score_list
-
-# determine the maximum score
-    def maximum(score1):
-        y = 0
-        for i in score1:
-            ind_score = int(i)
-            if ind_score > y:
-                y = ind_score
-        return y
-
-# determine the GPA
-    scores = student_score(num_students)
-    best = maximum(scores)
-    x = 0
-    student_number = 1
-    while x < num_students:
-        a = int(scores[x])
-        if a >= (best - 10):
-            print(f'Student {student_number} score is {a} and grade is A')
-        elif a >= (best - 20):
-            print(f'Student {student_number} score is {a} and grade is B')
-        elif a >= (best - 30):
-            print(f'Student {student_number} score is {a} and grade is C')
-        elif a >= (best - 40):
-            print(f'Student {student_number} score is {a} and grade is D')
-        else:
-            print(f'Student {student_number} score is {a} and grade is F')
-        x += 1
-        student_number += 1
 
 # submitting the student information
 # count number of students as student information is submitted
@@ -74,16 +31,61 @@ class Logic (QMainWindow, Ui_MainWindow):
             final = int((self.input_final.text()))
             final_score = (midterm + final)//2
             self.score_list.append(final_score)
-
-            row = [last_name, first_name, grade, midterm, final, final_grade,gpa]
-            with open('grade_info.csv', 'a', newline="") as info:
-                write_info = writer(info)
-                write_info.writerow(row)
-                info.close()
+            return last_name,first_name,grade,midterm,final,final_score
         except:
             self.Error_message.insertPlainText(f'Please input the correct information.')
         self.student_num += 1
         self.clear()
+
+        # returning a score list to use it to determining GPA
+        def student_gpa(self):
+            student_number = self.student_num
+            # score = input(f'Enter {student_num} score(s): ')
+            score_list = score.split()
+            if len(score_list) > student_number:
+                score_list = score_list[:student_number]
+            else:
+                while len(score_list) < student_number:
+                    score = input(f'Enter {student_number} score(s): ')
+                    score_list = score.split()
+            return score_list
+
+        # determine the maximum score
+        def maximum(score1):
+            y = 0
+            for i in score1:
+                ind_score = int(i)
+                if ind_score > y:
+                    y = ind_score
+            return y
+
+        # determine the GPA
+        def determine_gpa(self):
+            scores = student_score(num_students)
+            best = maximum(scores)
+            x = 0
+            student_number = 1
+            while x < num_students:
+                a = int(scores[x])
+                if a >= (best - 10):
+                    print(f'Student {student_number} score is {a} and grade is A')
+                elif a >= (best - 20):
+                    print(f'Student {student_number} score is {a} and grade is B')
+                elif a >= (best - 30):
+                    print(f'Student {student_number} score is {a} and grade is C')
+                elif a >= (best - 40):
+                    print(f'Student {student_number} score is {a} and grade is D')
+                else:
+                    print(f'Student {student_number} score is {a} and grade is F')
+                x += 1
+                student_number += 1
+
+    def write_csv_file(self,last_name,first_name,grade,midterm,final,final_score,gpa):
+        row = [last_name, first_name, grade, midterm, final, final_score, gpa]
+        with open('grade_info.csv', 'a', newline="") as info:
+            write_info = writer(info)
+            write_info.writerow(row)
+            info.close()
 
 
 # final submission/ display the GPA for each student
