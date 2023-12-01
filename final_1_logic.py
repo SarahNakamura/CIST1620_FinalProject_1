35d5e4ac0eacd10896fc0a074e970c35f3e9a411
SarahNakamura <snakamura@unomaha.edu>
2023-11-30 21:50:38 -0600

    update_9:50

--- final_1_logic.py
+++ final_1_logic.py
@@ -38,47 +38,47 @@ class Logic (QMainWindow, Ui_MainWindow):
         self.clear()
 
         # returning a score list to use it to determining GPA
-        def student_gpa(self):
-            student_number = self.student_num
+        #def student_gpa(self):
+            #student_number = self.student_num
             # score = input(f'Enter {student_num} score(s): ')
-            score_list = score.split()
-            if len(score_list) > student_number:
-                score_list = score_list[:student_number]
-            else:
-                while len(score_list) < student_number:
-                    score = input(f'Enter {student_number} score(s): ')
-                    score_list = score.split()
-            return score_list
+            #score_list = score.split()
+            #if len(score_list) > student_number:
+                #score_list = score_list[:student_number]
+            #else:
+                #while len(score_list) < student_number:
+                    #score = input(f'Enter {student_number} score(s): ')
+                    #score_list = score.split()
+            #return score_list
 
         # determine the maximum score
-        def maximum(score1):
-            y = 0
-            for i in score1:
-                ind_score = int(i)
-                if ind_score > y:
-                    y = ind_score
-            return y
+    def maximum(self,):
+        y = 0
+        for i in score1:
+            ind_score = int(i)
+            if ind_score > y:
+                y = ind_score
+        return y
 
         # determine the GPA
-        def determine_gpa(self):
-            scores = student_score(num_students)
-            best = maximum(scores)
-            x = 0
-            student_number = 1
-            while x < num_students:
-                a = int(scores[x])
-                if a >= (best - 10):
-                    print(f'Student {student_number} score is {a} and grade is A')
-                elif a >= (best - 20):
-                    print(f'Student {student_number} score is {a} and grade is B')
-                elif a >= (best - 30):
-                    print(f'Student {student_number} score is {a} and grade is C')
-                elif a >= (best - 40):
-                    print(f'Student {student_number} score is {a} and grade is D')
-                else:
-                    print(f'Student {student_number} score is {a} and grade is F')
-                x += 1
-                student_number += 1
+    def determine_gpa(self):
+        scores = student_score(num_students)
+        best = maximum(scores)
+        x = 0
+        student_number = 1
+        while x < num_students:
+            a = int(scores[x])
+            if a >= (best - 10):
+                print(f'Student {student_number} score is {a} and grade is A')
+            elif a >= (best - 20):
+                print(f'Student {student_number} score is {a} and grade is B')
+            elif a >= (best - 30):
+                print(f'Student {student_number} score is {a} and grade is C')
+            elif a >= (best - 40):
+                print(f'Student {student_number} score is {a} and grade is D')
+            else:
+                print(f'Student {student_number} score is {a} and grade is F')
+            x += 1
+            student_number += 1
 
     def write_csv_file(self,last_name,first_name,grade,midterm,final,final_score,gpa):
         row = [last_name, first_name, grade, midterm, final, final_score, gpa]
